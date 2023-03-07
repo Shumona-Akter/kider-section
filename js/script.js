8b77913b5758b252cd2418ff0cef42a0b50650f4
SHumona-Akter <shumona.cse.bd@gmail.com>
2023-03-07 08:36:30 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+$('.variable-width').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    arrows: true
+  });
+          
